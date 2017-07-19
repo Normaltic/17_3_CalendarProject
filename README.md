040e1e1245b1675d70765e29fb37d95d4a884f27
Normaltic <normaltic@naver.com>
2017-07-19 15:39:51 +0900

    initial states

---
+++ README.md
@@ -0,0 +1,17 @@
+# CalendarProject_ front-end
+
+캘린더앱 개발때 만든 API를 사용, React 프론트앤드 개발<br />
+
+API : https://github.com/Normaltic/17_CalendarServer
+
+~~~
+"start": "webpack-dev-server --hot",
+"build": "react-scripts build",
+"production": "babel server"
+~~~
+
+# Version
+
+## 1.0
+기본 라이브러리 설치 <br />
+기본 설정 ( scripts, webpack.config.js )
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 3001,
+        contentBase: __dirname + '/public/'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /mode_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
