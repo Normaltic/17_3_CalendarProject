484efed9d5006a6b35c3c19d7dad59a8cbee0a69
Normaltic <normaltic@naver.com>
2017-08-14 15:49:04 +0900

    initial states

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 3001,
+        contentBase: __dirname + '/public/',
+        historyApiFallback: true
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /mode_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    }
+};
