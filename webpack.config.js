36713f3ddbf64f5fdab2a0de013ae59b03f83608
Normal <normaltic@naver.com>
2017-12-27 10:25:13 +0000

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,25 @@
 module.exports = {
-    entry: './src/index.js',
+	entry: './src/index.js',
 
-    output: {
-        path: __dirname + '/public/',
-        filename: 'bundle.js'
-    },
+	output: {
+		path: __dirname + '/public/',
+		filename: 'bundle.js'
+	},
 
-    devServer: {
-        inline: true,
-        port: 3001,
-        contentBase: __dirname + '/public/',
-        historyApiFallback: true
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
-            },
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loaders: ['babel-loader?' +JSON.stringify({
+					cacheDirectory: true,
+					presets: ['es2015', 'react']
+				})],
+				exclude: /node_modules/,
+			},
             {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader'
             }
-        ]
-    }
+		]
+	}
 };
